da640482ef01273408261180e4bf6607cd37b995
berkkaradas <berk.karadas@bilgiedu.net>
2021-06-20 16:37:17 +0300

    Main Codes

---
+++ OnlineChat/profil2.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace OnlineChat
+{
+    partial class profil2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(profil2));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_yeni = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.tb_eski = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.btn_giris = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 40;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(125, 195);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(97, 19);
+            this.label10.TabIndex = 63;
+            this.label10.Text = "Yüklenemedi";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(125, 151);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(97, 19);
+            this.label9.TabIndex = 62;
+            this.label9.Text = "Yüklenemedi";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(125, 107);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(97, 19);
+            this.label8.TabIndex = 61;
+            this.label8.Text = "Yüklenemedi";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(125, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 19);
+            this.label7.TabIndex = 60;
+            this.label7.Text = "Yüklenemedi";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(125, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 19);
+            this.label6.TabIndex = 59;
+            this.label6.Text = "Yüklenemedi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 19);
+            this.label5.TabIndex = 58;
+            this.label5.Text = "Cinsiyet :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 19);
+            this.label4.TabIndex = 57;
+            this.label4.Text = "E Posta :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 19);
+            this.label3.TabIndex = 56;
+            this.label3.Text = "Soyadı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 19);
+            this.label2.TabIndex = 55;
+            this.label2.Text = "Adı :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 19);
+            this.label1.TabIndex = 54;
+            this.label1.Text = "İD:";
+            // 
+            // tb_yeni
+            // 
+            this.tb_yeni.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_yeni.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tb_yeni.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tb_yeni.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tb_yeni.HintForeColor = System.Drawing.Color.DimGray;
+            this.tb_yeni.HintText = "Yeni Şifre";
+            this.tb_yeni.isPassword = false;
+            this.tb_yeni.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(41)))), ((int)(((byte)(91)))));
+            this.tb_yeni.LineIdleColor = System.Drawing.Color.Gray;
+            this.tb_yeni.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(41)))), ((int)(((byte)(91)))));
+            this.tb_yeni.LineThickness = 3;
+            this.tb_yeni.Location = new System.Drawing.Point(27, 294);
+            this.tb_yeni.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_yeni.Name = "tb_yeni";
+            this.tb_yeni.Size = new System.Drawing.Size(147, 37);
+            this.tb_yeni.TabIndex = 53;
+            this.tb_yeni.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.tb_yeni.OnValueChanged += new System.EventHandler(this.tb_yeni_OnValueChanged);
+            // 
+            // tb_eski
+            // 
+            this.tb_eski.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_eski.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tb_eski.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tb_eski.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tb_eski.HintForeColor = System.Drawing.Color.DimGray;
+            this.tb_eski.HintText = "Eski Şifre";
+            this.tb_eski.isPassword = false;
+            this.tb_eski.LineFocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(41)))), ((int)(((byte)(91)))));
+            this.tb_eski.LineIdleColor = System.Drawing.Color.Gray;
+            this.tb_eski.LineMouseHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(41)))), ((int)(((byte)(91)))));
+            this.tb_eski.LineThickness = 3;
+            this.tb_eski.Location = new System.Drawing.Point(27, 249);
+            this.tb_eski.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_eski.Name = "tb_eski";
+            this.tb_eski.Size = new System.Drawing.Size(147, 37);
+            this.tb_eski.TabIndex = 52;
+            this.tb_eski.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.tb_eski.OnValueChanged += new System.EventHandler(this.tb_eski_OnValueChanged);
+            // 
+            // btn_giris
+            // 
+            this.btn_giris.ActiveBorderThickness = 1;
+            this.btn_giris.ActiveCornerRadius = 20;
+            this.btn_giris.ActiveFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(41)))), ((int)(((byte)(91)))));
+            this.btn_giris.ActiveForecolor = System.Drawing.Color.Silver;
+            this.btn_giris.ActiveLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(41)))), ((int)(((byte)(91)))));
+            this.btn_giris.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btn_giris.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(28)))), ((int)(((byte)(49)))));
+            this.btn_giris.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_giris.BackgroundImage")));
+            this.btn_giris.ButtonText = "Onayla";
+            this.btn_giris.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_giris.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_giris.ForeColor = System.Drawing.Color.White;
+            this.btn_giris.IdleBorderThickness = 1;
+            this.btn_giris.IdleCornerRadius = 20;
+            this.btn_giris.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(41)))), ((int)(((byte)(91)))));
+            this.btn_giris.IdleForecolor = System.Drawing.Color.White;
+            this.btn_giris.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(202)))), ((int)(((byte)(41)))), ((int)(((byte)(91)))));
+            this.btn_giris.Location = new System.Drawing.Point(214, 286);
+            this.btn_giris.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.btn_giris.Name = "btn_giris";
+            this.btn_giris.Size = new System.Drawing.Size(60, 45);
+            this.btn_giris.TabIndex = 51;
+            this.btn_giris.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btn_giris.Click += new System.EventHandler(this.btn_giris_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(122, 235);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(97, 19);
+            this.label11.TabIndex = 65;
+            this.label11.Text = "Yüklenemedi";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(20, 235);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(96, 19);
+            this.label12.TabIndex = 64;
+            this.label12.Text = "Vatandaşlık :";
+            // 
+            // profil2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(28)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(353, 372);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_yeni);
+            this.Controls.Add(this.tb_eski);
+            this.Controls.Add(this.btn_giris);
+            this.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "profil2";
+            this.Text = "profil2";
+            this.Load += new System.EventHandler(this.profil2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox tb_yeni;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox tb_eski;
+        private Bunifu.Framework.UI.BunifuThinButton2 btn_giris;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
